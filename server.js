ff62bf8dbe7b3be0253f7ae13a2c28fab8012f86
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-08-24 13:09:04 +0530

    refactor: restructure MongoDB connection and improve server setup

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 import express from "express";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import dotenv from "dotenv";
 import urlRoutes from "./routes/urlRoutes.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-// ✅ Apply Arcjet rate limiter
+// Apply Arcjet rate limiter
 app.use(rateLimiter);
+// Connect to MongoDB
+connectDB();
 
-// ✅ MongoDB connection
-mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("MongoDB connected"))
-	.catch((err) => console.error(err));
-
-// ✅ Routes
+//  Routes
 app.use("/", urlRoutes);
 
-// ✅ Start server
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
