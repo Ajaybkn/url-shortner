a54ae41bd15e53523548aa57dc731aaba5b9095f
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-09-02 16:05:35 +0530

    update CORS origin for local development

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: "https://url-shortner-ouml.onrender.com",
+		origin: "http://localhost:5173",
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 		credentials: true,
 	})
